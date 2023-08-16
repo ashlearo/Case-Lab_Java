ca095ebe89c9463f469a10043da3ab8ba93b29bf
Александр <99829795+ashlearo@users.noreply.github.com>
2023-08-16 20:55:46 +0300

    Update task3.java
    task3

--- src/task3.java
+++ src/task3.java
@@ -1,1 +1,13 @@
-task3
+    public static Node reverse(Node head)
+    {
+        Node previous = null;
+        Node current = head;
+        while (current != null)
+        {
+            Node next = current.next; 
+            current.next = previous;     
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
